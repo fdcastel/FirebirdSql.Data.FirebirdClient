8c7c76ec4f3879ab04bfb523e763e24a16fe1ca7
F.D.Castel <fdcastel@gmail.com>
2023-07-13 16:35:35 -0300

    Adds download-firebird-binaries.ps1.

---
+++ download-firebird-binaries.ps1
@@ -0,0 +1,9 @@
+$binariesFolder = "$env:TEMP/firebird-binaries"
+
+# Download Firebird binaries
+Remove-Item $binariesFolder -Recurse -Force -ErrorAction SilentlyContinue
+git clone --quiet --depth 1 --single-branch https://github.com/fdcastel/firebird-binaries $binariesFolder
+
+# Rebuild databases
+# & $binariesFolder/Rebuild-Databases.ps1 -Verbose
+
